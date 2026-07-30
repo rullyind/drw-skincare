d975bd049f9126f8d6e826c6be58f8a8eebc3174
rullyind <rully7773@gmail.com>
2026-07-30 11:29:00 +0700

    Update produk.js

--- produk.js
+++ produk.js
@@ -1,63 +1,39 @@
-const produk = [
+const produk=[
 
 {
-    id:1,
-    nama:"Sunscreen Oily & Acne",
-    harga:100000,
-    gambar:"images/Sunscreen Oily & Acne.png",
-    kategori:"Acne",
-    badge:"NEW",
-    deskripsi:"Membantu mencerahkan kulit dan mengurangi bekas jerawat."
+
+id:1,
+
+nama:"Sunscreen Oily Acne",
+
+harga:125000,
+
+gambar:"images/produk/sunscreen.png",
+
+kategori:"Cream Siang",
+
+badge:"BEST SELLER",
+
+deskripsi:"Melindungi kulit dari sinar UV."
+
 },
 
 {
-    id:2,
-    nama:"Day Cream Acne 1",
-    harga:100000,
-    gambar:"images/Day Cream Acne 1 TO.png",
-    kategori:"Acne",
-    badge:"NEW",
-    deskripsi:"Membantu mencerahkan kulit dan mengurangi bekas jerawat."
-},
 
-{
-    id:3,
-    nama:"Day Cream Acne 2",
-    harga:100000,
-    gambar:"images/Day Cream Acne 2 TO.png",
-    kategori:"Cream Siang",
-    badge:"Best Foundation",
-    deskripsi:"Membantu mencerahkan kulit dan mengurangi bekas jerawat."
-},
+id:2,
 
-{
-    id:4,
-    nama:"Sunscreen Glowing",
-    harga:100000,
-    gambar:"images/facialwash.png",
-    kategori:"Cleanser",
-    badge:"",
-    deskripsi:"Membersihkan wajah secara lembut."
-},
+nama:"Day Cream Acne",
 
-{
-    id:5,
-    nama:"Brightening Toner",
-    harga:99000,
-    gambar:"images/toner.png",
-    kategori:"Brightening",
-    badge:"",
-    deskripsi:"Membantu menyegarkan dan mencerahkan kulit."
-},
+harga:95000,
 
-{
-    id:6,
-    nama:"Whitening Serum",
-    harga:175000,
-    gambar:"images/serum.png",
-    kategori:"Serum",
-    badge:"HOT",
-    deskripsi:"Serum premium untuk kulit lebih cerah."
+gambar:"images/produk/daycream.png",
+
+kategori:"Cream Siang",
+
+badge:"NEW",
+
+deskripsi:"Mencerahkan kulit."
+
 }
 
 ];
