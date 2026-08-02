d4f50cb9346d93f6618cd94c78eda7c69488e4a8
rullyind <rully7773@gmail.com>
2026-08-02 23:58:09 +0700

    Update Website DRW Skincare

--- assets/js/products-data.js
+++ assets/js/products-data.js
@@ -10,7 +10,7 @@ const DRW_PRODUCTS = [
         name: "Sunscreen Oily & Acne",
         category: "Cream Siang",
         price: 105000,
-        image: "assets/images/products/Sunscreen Oily & Acne.png",
+        image: "assets/images/products/Cream Siang/Sunscreen Oily & Acne/Sunscreen Oily & Acne TO.png",
         description:
             "Perawatan skincare untuk melengkapi rutinitas harian dan membantu menjaga kulit tetap terasa nyaman, lembut, dan sehat."
     },
@@ -20,7 +20,7 @@ const DRW_PRODUCTS = [
         name: "Day Cream Acne 1",
         category: "Cream Siang",
         price: 100000,
-        image: "assets/images/products/Day Cream Acne 1 TO.png",
+        image: "assets/images/products/Cream Siang/Day Cream Acne 1/Day Cream Acne 1 TO.png",
         description:
             "Cream siang untuk membantu kulit tampak lebih cerah, segar, dan terawat."
     },
@@ -30,7 +30,7 @@ const DRW_PRODUCTS = [
         name: "Day Cream Acne 2",
         category: "Cream Siang",
         price: 100000,
-        image: "assets/images/products/Day Cream Acne 2 TO.png",
+        image: "assets/images/products/Cream Siang/Day Cream Acne 2/Day Cream Acne 2 TO.png",
         description:
             "Cream siang untuk membantu menjaga kelembapan kulit agar terasa lembut dan nyaman."
     }
