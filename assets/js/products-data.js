69f1edb38bf4f3670cefab2fd597610f1500dc1d
rulles_ind <rully7773@gmail.com>
2026-09-21 13:02:10 +0700

    Update products-data.js

--- assets/js/products-data.js
+++ assets/js/products-data.js
@@ -55,16 +55,6 @@
             reviews: 92
         },
 
-
-           {
-            id: "radiant-glow-platinum",
-            name: "Radiant Glow Platinum",
-            category: "Cream Malam",
-            image: IMAGE_FOLDER + "Radiant Glow Platinum.png",
-            price: 115000,
-            rating: 4.9,
-            reviews: 51,
-        },
         
         {
             id: "bamboo-charcoal-soap-premium",
@@ -608,6 +598,16 @@
             image: IMAGE_FOLDER + "Moisture Gel Cucumber.jpg",
             price: 80000,
             
+        },
+
+        {
+            id: "radiant-glow-platinum",
+            name: "Radiant Glow Platinum",
+            category: "Cream Malam",
+            image: IMAGE_FOLDER + "Radiant Glow Platinum.png",
+            price: 115000,
+            rating: 4.9,
+            reviews: 51,
         },
 
 
